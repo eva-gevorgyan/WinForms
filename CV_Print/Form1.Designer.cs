4f02efdb6252fe0232414ff6ba9e133817c464d7
eva-gevorgyan <127292682+eva-gevorgyan@users.noreply.github.com>
2023-06-01 17:29:18 +0400

    Add files via upload

---
+++ CV_Print/Form1.Designer.cs
@@ -0,0 +1,196 @@
+﻿namespace CV_Print
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            textBox1 = new TextBox();
+            textBox2 = new TextBox();
+            textBox3 = new TextBox();
+            textBox4 = new TextBox();
+            textBox5 = new TextBox();
+            label1 = new Label();
+            label2 = new Label();
+            label3 = new Label();
+            label4 = new Label();
+            label5 = new Label();
+            pictureBox1 = new PictureBox();
+            button1 = new Button();
+            button2 = new Button();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            SuspendLayout();
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(404, 25);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(125, 27);
+            textBox1.TabIndex = 0;
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(404, 82);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(125, 27);
+            textBox2.TabIndex = 1;
+            // 
+            // textBox3
+            // 
+            textBox3.Location = new Point(404, 135);
+            textBox3.Name = "textBox3";
+            textBox3.Size = new Size(125, 27);
+            textBox3.TabIndex = 2;
+            // 
+            // textBox4
+            // 
+            textBox4.Location = new Point(404, 189);
+            textBox4.Name = "textBox4";
+            textBox4.Size = new Size(125, 27);
+            textBox4.TabIndex = 3;
+            // 
+            // textBox5
+            // 
+            textBox5.Location = new Point(404, 241);
+            textBox5.Name = "textBox5";
+            textBox5.Size = new Size(125, 27);
+            textBox5.TabIndex = 4;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(325, 25);
+            label1.Name = "label1";
+            label1.Size = new Size(49, 20);
+            label1.TabIndex = 5;
+            label1.Text = "Name";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(325, 82);
+            label2.Name = "label2";
+            label2.Size = new Size(67, 20);
+            label2.TabIndex = 5;
+            label2.Text = "Surname";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(325, 135);
+            label3.Name = "label3";
+            label3.Size = new Size(50, 20);
+            label3.TabIndex = 5;
+            label3.Text = "Phone";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(325, 189);
+            label4.Name = "label4";
+            label4.Size = new Size(46, 20);
+            label4.TabIndex = 5;
+            label4.Text = "Email";
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Location = new Point(325, 248);
+            label5.Name = "label5";
+            label5.Size = new Size(53, 20);
+            label5.TabIndex = 5;
+            label5.Text = "Adress";
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Location = new Point(32, 12);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(237, 197);
+            pictureBox1.TabIndex = 6;
+            pictureBox1.TabStop = false;
+            // 
+            // button1
+            // 
+            button1.Location = new Point(74, 225);
+            button1.Name = "button1";
+            button1.Size = new Size(118, 29);
+            button1.TabIndex = 7;
+            button1.Text = "Upload Image";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(276, 359);
+            button2.Name = "button2";
+            button2.Size = new Size(184, 42);
+            button2.TabIndex = 8;
+            button2.Text = "Save As PDF";
+            button2.UseVisualStyleBackColor = true;
+            button2.Click += button2_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            Controls.Add(pictureBox1);
+            Controls.Add(label5);
+            Controls.Add(label4);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Controls.Add(textBox5);
+            Controls.Add(textBox4);
+            Controls.Add(textBox3);
+            Controls.Add(textBox2);
+            Controls.Add(textBox1);
+            Name = "Form1";
+            Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private TextBox textBox1;
+        private TextBox textBox2;
+        private TextBox textBox3;
+        private TextBox textBox4;
+        private TextBox textBox5;
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private Label label4;
+        private Label label5;
+        private PictureBox pictureBox1;
+        private Button button1;
+        private Button button2;
+    }
+}
---
+++ CV_Print/Form1.cs
@@ -0,0 +1,73 @@
+﻿using System.Drawing.Printing;
+
+namespace CV_Print
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private Image cvImage;
+        private void button1_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "Image Files (*.jpg,*.jpeg)|*.jpg;*.jpeg";
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                cvImage = Image.FromFile(openFileDialog.FileName);
+                cvImage = ResizeImage(cvImage, pictureBox1.Width, pictureBox1.Height);
+                pictureBox1.Image = cvImage;
+            }
+        }
+
+        private Image ResizeImage(Image image, int width, int height)
+        {
+            Bitmap resizedImage = new Bitmap(width, height);
+            using (Graphics graphics = Graphics.FromImage(resizedImage))
+            {
+                graphics.DrawImage(image, 0, 0, width, height);
+            }
+            return resizedImage;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            PrintDocument printDocument = new PrintDocument();
+            printDocument.DocumentName = "CV";
+            printDocument.PrintPage += new PrintPageEventHandler(pd_PrintPage);
+
+            PrintDialog printDialog = new PrintDialog();
+            printDialog.Document = printDocument;
+
+            if (printDialog.ShowDialog() == DialogResult.OK)
+            {
+                printDocument.Print();
+            }
+        }
+
+        void pd_PrintPage(object sender, PrintPageEventArgs ev)
+        {
+            string name = textBox1.Text;
+            string surname = textBox2.Text;
+            string phone = textBox3.Text;
+            string email = textBox4.Text;
+            string address = textBox5.Text;
+
+            if (cvImage != null)
+            {
+                ev.Graphics.DrawImage(cvImage, 10, 10);
+            }
+
+            Font font = new Font("Arial", 12);
+            Brush brush = Brushes.Red;
+            ev.Graphics.DrawString("Name: " + name, font, brush, 10, 50 + cvImage.Height);
+            ev.Graphics.DrawString("Surname: " + surname, font, brush, 10, 70 + cvImage.Height);
+            ev.Graphics.DrawString("Phone: " + phone, font, brush, 10, 90 + cvImage.Height);
+            ev.Graphics.DrawString("Email: " + email, font, brush, 10, 110 + cvImage.Height);
+            ev.Graphics.DrawString("Address: " + address, font, brush, 10, 130 + cvImage.Height);
+        }
+
+    }
+}
